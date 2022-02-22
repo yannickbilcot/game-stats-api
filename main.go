0b81838f1de47b8649aced9ddadd39015340ba39
Yannick BILCOT <yannick.bilcot@laposte.net>
2022-02-22 22:59:16 +0800

    feat: serve spa ui frontend

--- main.go
+++ main.go
@@ -11,9 +11,21 @@ import (
 	"github.com/go-chi/chi/middleware"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load()
+
+	address := os.Getenv("ADDRESS")
+	if address == "" {
+		address = "127.0.0.1"
+	}
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
 	err := database.Initialize("data.db")
 	if err != nil {
 		log.Println(err)
@@ -22,24 +34,29 @@ func main() {
 
 	router := mux.NewRouter()
 	router.StrictSlash(true)
-	router.HandleFunc("/games/", server.CreateGameHandler).Methods("POST")
-	router.HandleFunc("/games/", server.GetAllGamesHandler).Methods("GET")
-	router.HandleFunc("/games/{id:[0-9]+}/", server.GetGameHandler).Methods("GET")
-	router.HandleFunc("/games/{id:[0-9]+}/", server.DeleteGameHandler).Methods("DELETE")
-	router.HandleFunc("/games/{id:[0-9]+}/players/", server.CreateGamePlayerHandler).Methods("POST")
-	router.HandleFunc("/games/{gid:[0-9]+}/players/{pid:[0-9]+}/", server.DeleteGamePlayerHandler).Methods("DELETE")
-	router.HandleFunc("/games/{gid:[0-9]+}/players/{pid:[0-9]+}/stats/", server.AddGamePlayerStatHandler).Methods("POST")
-	router.HandleFunc("/games/{gid:[0-9]+}/players/{pid:[0-9]+}/laststat/", server.DeleteGamePlayerLastStatHandler).Methods("DELETE")
+	router.HandleFunc("/api/v1/games/", server.CreateGameHandler).Methods("POST")
+	router.HandleFunc("/api/v1/games/", server.GetAllGamesHandler).Methods("GET")
+	router.HandleFunc("/api/v1/games/{id:[0-9]+}/", server.GetGameHandler).Methods("GET")
+	router.HandleFunc("/api/v1/games/{id:[0-9]+}/", server.DeleteGameHandler).Methods("DELETE")
+	router.HandleFunc("/api/v1/games/{id:[0-9]+}/players/", server.CreateGamePlayerHandler).Methods("POST")
+	router.HandleFunc("/api/v1/games/{gid:[0-9]+}/players/{pid:[0-9]+}/", server.DeleteGamePlayerHandler).Methods("DELETE")
+	router.HandleFunc("/api/v1/games/{gid:[0-9]+}/players/{pid:[0-9]+}/stats/", server.AddGamePlayerStatHandler).Methods("POST")
+	router.HandleFunc("/api/v1/games/{gid:[0-9]+}/players/{pid:[0-9]+}/laststat/", server.DeleteGamePlayerLastStatHandler).Methods("DELETE")
 	router.Use(middleware.Logger)
 	router.Use(middleware.AllowContentEncoding("application/json"))
+
+	spa := server.NewSpaHandler("ui/dist/spa", "index.html")
+	router.PathPrefix("/").Handler(spa)
+
 	allowedHeaders := handlers.AllowedHeaders([]string{"X-Requested-With","content-type"})
 	allowedOrigins := handlers.AllowedOrigins([]string{"*"})
 	allowedMethods := handlers.AllowedMethods([]string{"GET", "POST", "DELETE", "OPTIONS"})
-	log.Println("Listening...")
+
+	log.Printf("Listening on %v:%v", address, port)
 
 	srv := &http.Server{
 		Handler:      handlers.CORS(allowedOrigins, allowedHeaders, allowedMethods)(router),
-		Addr:         "127.0.0.1:3000",
+		Addr:         address + ":" + port,
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
